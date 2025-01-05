65ec785292da0d9bd4dad5be4a35a297f74eb829
Nick Palenchar <nickpal@nickpalenchar.com>
2025-01-05 11:47:22 -0500

    remove debug

--- server/client-scripts/fullScreenToggle.js
+++ server/client-scripts/fullScreenToggle.js
@@ -88,8 +88,6 @@ window.fullScreenButton = {
     function lockScreen() {
       if ("wakeLock" in navigator) {
         navigator.wakeLock.request("screen").then((lock) => {
-          console.log('requestedddddd')
-          alert('debug - screen lock successful');
           wakeLock = lock
           wakeLock.addEventListener('release', () => {
             console.log('wakeLock released')
@@ -97,19 +95,16 @@ window.fullScreenButton = {
         })
       }
       if ('screen' in window && 'keepAwake' in window.screen) {
-        alert('debug - screen lock for older browsers')
         window.screen.keepAwake = true
       }
     }
     function unlockScreen() {
       if ("wakeLock" in navigator && wakeLock) {
         wakeLock.release().then(() => {
-          alert('debug - screen unlock successful');
           wakeLock = null;
         })
       }
       if ('screen' in window && 'keepAwake' in window.screen) {
-        alert('debug - screen unlock for older browsers')
         window.screen.keepAwake = false
       }
     }
